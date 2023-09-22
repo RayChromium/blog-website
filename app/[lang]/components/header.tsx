99c4a2e9eeeeb073af6a9dd75f543c18b6920ee8
RayChromium <raychromium@outlook.com>
2023-09-23 00:18:30 +0300

    header and button color

--- app/[lang]/components/header.tsx
+++ app/[lang]/components/header.tsx
@@ -8,19 +8,25 @@ export default async function Header({ lang }: { lang: Locale }) {
   const { navigation } = await getDictionary(lang)
 
   return (
-    <header className='py-6 bg-cyan-100'>
+    <header className='py-6 bg-blue-800 text-white shadow-md'>
       <nav className='relative container mx-auto p-6'>
         <div className="flex items-center justify-between space-x-4">
-        <div className="pt-2">
-          <Link href={`/${lang}`} className='hover:text-cyan-950'>
-            <Image src={logo} alt="cool logo"/>
-          </Link>
-        </div>
-          <Link href={`/${lang}`} className='hover:text-cyan-950 rounded-lg hover:shadow-lg transform hover:scale-95 transition-all duration-200 ease-in-out'>{navigation.home}</Link>
-          <Link href={`/${lang}/about`} className='hover:text-cyan-950 rounded-lg hover:shadow-lg transform hover:scale-95 transition-all duration-200 ease-in-out'>{navigation.about}</Link>
-          <Link href={`/${lang}/blogs`} className='hover:text-cyan-950 rounded-lg hover:shadow-lg transform hover:scale-95 transition-all duration-200 ease-in-out'>{navigation.blogs}</Link>
-        </div>
-      </nav>
-    </header>
+          <div className="pt-2">
+            <Link href={`/${lang}`} className='hover:text-blue-300'>
+              <Image src={logo} alt="cool logo"/>
+            </Link>
+          </div>
+      <Link href={`/${lang}`} className='hover:text-blue-300 hover:bg-white hover:bg-opacity-10 rounded-lg px-2 py-1 transition-all duration-200 ease-in-out'>
+        {navigation.home}
+      </Link>
+      <Link href={`/${lang}/about`} className='hover:text-blue-300 hover:bg-white hover:bg-opacity-10 rounded-lg px-2 py-1 transition-all duration-200 ease-in-out'>
+        {navigation.about}
+      </Link>
+      <Link href={`/${lang}/blogs`} className='hover:text-blue-300 hover:bg-white hover:bg-opacity-10 rounded-lg px-2 py-1 transition-all duration-200 ease-in-out'>
+        {navigation.blogs}
+      </Link>
+    </div>
+  </nav>
+</header>
   )
 }
